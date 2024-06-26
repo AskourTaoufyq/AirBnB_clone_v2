0a0c8e05c7413ec6a89004a2db93f32de133dbd2
AskourTaoufyq <136017759+AskourTaoufyq@users.noreply.github.com>
2024-06-26 13:34:34 +0100

    AirBnB V2

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define the amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class amenity(BaseModel, Base):
+    """represents an Amenity for a MySQL database."""
+
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
